d905ae14075e94e6bee2a3494758f57dcb422499
downback <daeun.down.back@gmail.com>
2025-04-08 16:42:57 +0200

    deploy text final4

--- src/app/(content)/consulting/page.tsx
+++ src/app/(content)/consulting/page.tsx
@@ -1,23 +1,22 @@
 "use client"
 
-import React, { useEffect } from "react"
-import { useSearchParams } from "next/navigation"
+import React from "react"
 import ConsultingHeader from "./_components/ConsultingHeader"
 import StraightLineProcess from "./_components/StraightLineProcess"
 
 const ConsultingPage: React.FC = () => {
-  const searchParams = useSearchParams()
+  // const searchParams = useSearchParams()
 
-  useEffect(() => {
-    const sectionParam = searchParams.get("section")
+  // useEffect(() => {
+  //   const sectionParam = searchParams.get("section")
 
-    if (!sectionParam) {
-      // Delay scroll to top slightly to avoid layout jank
-      requestAnimationFrame(() => {
-        window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
-      })
-    }
-  }, [searchParams])
+  //   if (!sectionParam) {
+  //     // Delay scroll to top slightly to avoid layout jank
+  //     requestAnimationFrame(() => {
+  //       window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
+  //     })
+  //   }
+  // }, [searchParams])
 
   return (
     <div className="flex flex-col items-center justify-center w-full min-h-screen">
