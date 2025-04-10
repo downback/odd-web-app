1c37448ee010e02c55d0742f3f6f2c32ff8adcce
downback <daeun.down.back@gmail.com>
2025-04-11 01:38:51 +0200

    deploy firebase-functions error

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from "firebase-functions"
+import * as admin from "firebase-admin";
 import express from "express"
 import { Request, Response, NextFunction } from "express"
 import cors from "cors"
