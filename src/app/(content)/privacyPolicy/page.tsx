1067ec263f97dba3d04f75b09617f7e10a747756
downback <daeun.down.back@gmail.com>
2025-04-25 17:51:13 +0200

    deploy try2

--- src/app/(content)/privacyPolicy/page.tsx
+++ src/app/(content)/privacyPolicy/page.tsx
@@ -4,7 +4,9 @@ import { LanguageContext } from "../../../context/LanguageContext"
 const PrivacyPolicyPage: React.FC = () => {
   const { translations } = useContext(LanguageContext)
   const privacyPolicyPageTranslation = translations.privacyPolicyPage
-  return <div className="w-full h-dvh"></div>
+  return (
+    <div className="w-full h-dvh">{privacyPolicyPageTranslation.title}</div>
+  )
 }
 
 export default PrivacyPolicyPage
